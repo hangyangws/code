234d65de129505185b3ffc112671dbef48b09189
hangyangws <hangyangws@qq.com>
2017-04-04 19:55:00 +0800

    - 面向对象 改造 searchPage

--- searchPage/static/js/public.js
+++ searchPage/static/js/public.js
@@ -1,31 +1,71 @@
-var getRandom = function(min, max) {
-        return Math.random() * (max - min) + min;
-    },
-    getWinWidth = function() {
-        return g.innerWidth || doc.documentElement.clientWidth || doc.body.clientWidth;
-    },
-    getWinHeight = function() {
-        return g.innerHeight || doc.documentElement.clientHeight || doc.body.clientHeight;
-    },
-    getColor = function() {
-        return 'rgba(RED, GREEN, BLUE, TRANS)'
-            .replace('RED', getRandom(0, 255).toFixed(0))
-            .replace('GREEN', getRandom(0, 255).toFixed(0))
-            .replace('BLUE', getRandom(0, 255).toFixed(0))
-            .replace('TRANS', .4);
-    };
-
-
-;
-! function(g, undefined) {
-    var RAF =
-        g.RAF ||
-        g.requestAnimationFrame ||
-        g.webkitRequestAnimationFrame ||
-        g.mozRequestAnimationFrame ||
-        g.oRequestAnimationFrame ||
-        g.msRequestAnimationFrame ||
-        function(callBack) {
-            g.setTimeout(callBack, 16.6);
+function getQueryObj() {
+    var _search = decodeURI(location.search.slice(1)),
+        _obj = {},
+        getTrueValue = function(_v) {
+            if (_v === 'false') {
+                return false;
+            }
+            if (_v === 'true') {
+                return true;
+            }
+            return _v;
         };
-}(this);
+    _search = _search.split('&');
+    var _l = _search.length,
+        _temp;
+    while (_l--) {
+        _temp = _search[_l].split('=');
+        _obj[_temp[0]] = getTrueValue(_temp[1]);
+    }
+    return _obj;
+}
+
+function cloneObj(obj) {
+    var newobj = obj.constructor === Array ? [] : {};
+    if (typeof obj !== 'object') {
+        return;
+    }
+    if (window.JSON) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+    for (var i in obj) {
+        newobj[i] = typeof obj[i] === 'object' ?
+            cloneObj(obj[i]) : obj[i];
+    }
+    return newobj;
+}
+
+! function(g, doc, undefined) {
+
+    if (!g.RAF) {
+        g.RAF =
+            g.requestAnimationFrame ||
+            g.webkitRequestAnimationFrame ||
+            g.mozRequestAnimationFrame ||
+            g.oRequestAnimationFrame ||
+            g.msRequestAnimationFrame ||
+            function(callBack) {
+                g.setTimeout(callBack, 16.6);
+            };
+    }
+
+    // 事件绑定函数
+    if (!g.addEvent) {
+        g.addEvent = (function() {
+            if (g.addEventListener) {
+                return function(element, type, handler) {
+                    element.addEventListener(type, handler, false);
+                }
+            }
+            if (g.attachEvent) {
+                return function(element, type, handler) {
+                    element.attachEvent('on' + type, handler);
+                }
+            }
+            return function(element, type, handler) {
+                element.addEventListener(type, handler, false);
+                element['on' + type] = handler;
+            }
+        })();
+    }
+}(this, document);

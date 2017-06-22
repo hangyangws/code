6ab3756f643d59b9a9d57a43cd43ba582d95f615
hangyangws <hangyangws@qq.com>
2017-06-22 17:49:33 +0800

    no message

--- audit-suse/js/jwjs.js
+++ audit-suse/js/jwjs.js
@@ -1,49 +1,49 @@
 ;
 (function($) {
 
-    var jwjs = (function() {
+  var jwjs = (function() {
 
-        var fir = $('.m-img-btn li:first')[0],
-            now = fir,
-            next;
+    var fir = $('.m-img-btn li:first')[0],
+      now = fir,
+      next;
 
-        return {
+    return {
 
-            gotoFixed: function(this$) {
-                var index = this$.siblings().removeClass('active').end().addClass('active').attr('data-index'),
-                    dom = $('.m-img').find("li[data-index=" + index + "]");
-                dom.siblings().removeClass('active').end().addClass('active');
-            },
+      gotoFixed: function(this$) {
+        var index = this$.siblings().removeClass('active').end().addClass('active').attr('data-index'),
+          dom = $('.m-img').find("li[data-index=" + index + "]");
+        dom.siblings().removeClass('active').end().addClass('active');
+      },
 
-            start: function() {
-                next = $(now).next()[0];
-                now = next ? next : fir;
-                jwjs.gotoFixed($(now));
-            }
-        };
-    }());
+      start: function() {
+        next = $(now).next()[0];
+        now = next ? next : fir;
+        jwjs.gotoFixed($(now));
+      }
+    };
+  }());
 
-    // gotoFixed
-    $('.m-img-btn li').bind('hover', function() {
-        jwjs.gotoFixed($(this));
-    });
+  // gotoFixed
+  $('.m-img-btn li').bind('hover', function() {
+    jwjs.gotoFixed($(this));
+  });
 
-    // info hovr change
-    // hy change
-    $('.info-each>div>ul>li').bind('click', function() {
-        $(this).parents('.info-each').find('h3 a')
-            .attr('href', 'list-' + $(this).find('span[data-i]').attr('data-i') + '.aspx')
-            .end().end()
-            .siblings().removeClass('live').end().addClass('live');
-    });
+  // info hovr change
+  // hy change
+  $('.info-each>div>ul>li').bind('click', function() {
+    $(this).parents('.info-each').find('h3 a')
+      .attr('href', 'list-' + $(this).find('span[data-i]').attr('data-i') + '.aspx')
+      .end().end()
+      .siblings().removeClass('live').end().addClass('live');
+  });
 
-    // cricle timeID
-    var time = setInterval(jwjs.start, 4000);
+  // cricle timeID
+  var time = setInterval(jwjs.start, 4000);
 
-    // carousel stop
-    $('.m-carousel').hover(function() {
-        clearInterval(time);
-    }, function() {
-        time = setInterval(jwjs.start, 4000);
-    });
+  // carousel stop
+  $('.m-carousel').hover(function() {
+    clearInterval(time);
+  }, function() {
+    time = setInterval(jwjs.start, 4000);
+  });
 })(jQuery);
